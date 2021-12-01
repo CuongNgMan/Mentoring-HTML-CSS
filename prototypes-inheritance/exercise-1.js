e1c8d7e69a11c7f344d951dc204bd99488b01f56
Cuong Ng M <cuong.ngman@gmail.com>
2021-12-01 20:05:42 +0700

    using Reflect.construct to mimic the new operator

--- prototypes-inheritance/exercise-1.js
+++ prototypes-inheritance/exercise-1.js
@@ -7,9 +7,9 @@ Person.prototype.introduce = function () {
   return 'My name is ' + this.name + ' and I am ' + this.age;
 };
 
-export function myNew(cons, ...args) {
+export function myNew(constructor, ...args) {
   try {
-    return new cons(...args);
+    return Reflect.construct(constructor, args);
   } catch (error) {
     throw new Error('Not a valid constructor');
   }
